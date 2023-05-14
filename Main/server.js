26cad10217abc2d4631434216daa3c196777ea4e
L E <erickson.leah.j@gmail.com>
2023-05-14 14:04:21 -0600

    input code to connect to database

--- Main/server.js
+++ Main/server.js
@@ -9,3 +9,14 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      database: 'employee_db',
+    },
+    console.log(`Connected to the employee_db database.`)
+);
