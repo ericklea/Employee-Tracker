c70c6701606c8d056141cff9be56a5cabf264d7b
L E <erickson.leah.j@gmail.com>
2023-05-14 13:20:03 -0600

    created employee table

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -18,4 +18,13 @@ CREATE TABLE role (
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-)
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    role_id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
