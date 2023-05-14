83c25e75f79d5e7c62a82644d8c5a6e95d2612ac
L E <erickson.leah.j@gmail.com>
2023-05-14 13:19:06 -0600

    created role table

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -8,3 +8,14 @@ CREATE TABLE department (
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    department_id INT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+)
