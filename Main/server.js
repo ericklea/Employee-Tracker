ddf1372dd16bce8c0a70649f192538328ece7304
L E <erickson.leah.j@gmail.com>
2023-05-14 12:56:03 -0600

    input code to import express mysql and inquirer

---
+++ Main/server.js
@@ -0,0 +1,7 @@
+// import express, mysql, and inquirer
+ const express = require('express');
+ const mysql = require('mysql2');
+ const inquirer = require('inquirer');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
