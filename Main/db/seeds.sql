3a82bcac51be65afda47d99504a360b5b397a072
L E <erickson.leah.j@gmail.com>
2023-05-14 13:08:29 -0600

    input code for role

--- Main/db/seeds.sql
+++ Main/db/seeds.sql
@@ -1,4 +1,13 @@
 INSERT INTO department (name)
 VALUES ("Engineering"),
        ("Finance"),
+       ("Sales"),
        ("Legal");
+
+INSERT INTO role (department_id, title, salary)
+VALUES ("1", "Software Engineer", "100000"),
+       ("1", "Lead Software Engineer", "150000"),
+       ("2", "Accountant", "80000"),
+       ("3", "Salesperson", "80000"),
+       ("3", "Sales Lead", "120000"),
+       ("4", "Lawyer", "100000");
