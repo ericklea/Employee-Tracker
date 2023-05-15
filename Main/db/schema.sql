4134c18d250a003f72842a66296266b25cfe774e
L E <erickson.leah.j@gmail.com>
2023-05-15 14:42:21 -0600

    updated schema.sql

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -25,6 +25,6 @@ CREATE TABLE employee (
     last_name VARCHAR(30),
     manager_id INT,
     PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- Main/db/seeds.sql
+++ Main/db/seeds.sql
@@ -5,23 +5,17 @@ VALUES ("Engineering"),
        ("Legal");
 
 INSERT INTO role (department_id, title, salary)
-VALUES ("1", "Software Engineer", "100000"),
-       ("1", "Lead Software Engineer", "150000"),
-       ("2", "Accountant", "80000"),
-       ("3", "Salesperson", "80000"),
-       ("3", "Sales Lead", "120000"),
-       ("4", "Lawyer", "100000");
+VALUES (1, "Software Engineer", "100000"),
+       (1, "Lead Software Engineer", "150000"),
+       (2, "Accountant", "80000"),
+       (3, "Salesperson", "80000"),
+       (3, "Sales Lead", "120000"),
+       (4, "Lawyer", "100000");
 
 INSERT INTO employee (role_id, first_name, last_name, manager_id)
-VALUES ("1", "John", "Doe", "2"),
-       ("2", "Jane", "Neuman", "2"),
-       ("3", "Bob", "Smith", "4"),
-       ("4", "Sally", "Erickson", "4"),
-       ("5", "Joe", "Nemeroff", "6"),
-       ("6", "Karen", "Shafley", "6"),
-       ("7", "Mark", "Fox", "8"),
-       ("8", "Mary", "Remmelts", "8"),
-       ("9", "Tom", "Jones", "10"),
-       ("10", "Sue", "Scott", "10"),
-       ("11", "Bill", "Gates", "12"),
-       ("12", "Steve", "Jobs", "12");
+VALUES (1, "John", "Doe", NULL),
+       (2, "Jane", "Neuman", NULL),
+       (3, "Bob", "Smith", NULL),
+       (4, "Sally", "Erickson", 1),
+       (5, "Joe", "Nemeroff", NULL);
+     
