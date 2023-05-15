a0f5c1eaffb04b498812c06bb1b3e989df47eaf7
L E <erickson.leah.j@gmail.com>
2023-05-15 14:00:00 -0600

    updated query.sql

--- Main/db/query.sql
+++ Main/db/query.sql
@@ -2,6 +2,6 @@ SELECT * FROM departments;
 SELECT * FROM roles;
 SELECT * FROM employees;
 INSERT INTO departments (department_name) VALUES ('Department Name');
-INSERT INTO roles (title, salary, department_id) VALUES ('Role Title', 50000, 1);
+INSERT INTO roles (title, salary, department_id) VALUES ('Role Title','salary','department_id' );
 INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('First Name', 'Last Name', 1, 1);
-UPDATE departments SET department_name = 'New Department Name' WHERE id = 1;
+UPDATE departments SET department_name = 'New Department Name' WHERE id ='';
