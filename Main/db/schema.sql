f2dede8e3c861b0582b80356ffbf3838a0be871c
L E <erickson.leah.j@gmail.com>
2023-05-15 13:57:21 -0600

    reorganized schema.sql

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -4,25 +4,25 @@ CREATE DATABASE tracker_db;
 USE tracker_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
-    department_id INT NOT NULL,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10,2) NOT NULL,
+    id INT,
+    department_id INT,
+    title VARCHAR(30),
+    salary DECIMAL,
     PRIMARY KEY (id),
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
-    role_id INT NOT NULL,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
+    id INT,
+    role_id INT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
     manager_id INT,
     PRIMARY KEY (id),
     FOREIGN KEY (role_id) REFERENCES role(id),
