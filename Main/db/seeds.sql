b418182e2967ca9b1b7f5d4dc847bdf5cdf7af75
L E <erickson.leah.j@gmail.com>
2023-05-15 16:52:12 -0600

    updated seeds.sql

--- Main/db/seeds.sql
+++ Main/db/seeds.sql
@@ -8,16 +8,17 @@ VALUES ("Engineering"),
 
 INSERT INTO role (department_id, title, salary)
 VALUES (1, "Software Engineer", "100000"),
-       (1, "Lead Software Engineer", "150000"),
-       (2, "Accountant", "80000"),
-       (3, "Salesperson", "80000"),
-       (3, "Sales Lead", "120000"),
-       (4, "Lawyer", "100000");
+       (2, "Lead Software Engineer", "150000"),
+       (3, "Accountant", "80000"),
+       (4, "Salesperson", "80000"),
+       (5, "Sales Lead", "120000"),
+       (6, "Lawyer", "100000");
 
 INSERT INTO employee (role_id, first_name, last_name, manager_id)
-VALUES (1, "John", "Doe", NULL),
+VALUES (1, "John", "Doe", 2),
        (2, "Jane", "Neuman", NULL),
        (3, "Bob", "Smith", NULL),
-       (4, "Sally", "Erickson", 1),
+       (4, "Sally", "Erickson", 5),
        (5, "Joe", "Nemeroff", NULL);
+       (6, "Bill", "Waters", NULL)
      
