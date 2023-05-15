c9bdd1ddc6b538893c072a947e9ea322d6af708a
L E <erickson.leah.j@gmail.com>
2023-05-15 17:20:24 -0600

    input code in index.js to connect to mysql server

--- Main/index.js
+++ Main/index.js
@@ -5,3 +5,13 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     database: 'tracker_db',
+});
+
+// Connect to the MySQL server
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to the tracker_db database.');
+}
+);
+
+
