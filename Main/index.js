9d365c75453ee6bc75456365fdc1d31d07160640
L E <erickson.leah.j@gmail.com>
2023-05-15 18:14:30 -0600

    input prompt to ask user questions

--- Main/index.js
+++ Main/index.js
@@ -2,4 +2,44 @@
 const inquirer = require('inquirer');
 const sql = require('mysql2');
 
-//
+// Prompt user for what they would like to do
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'What would you like to do?',
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Exit']
+        }
+    ])
+    .then((data) => {
+        switch (data.choice) {
+            case 'View all departments':
+                viewDepartments();
+                break;
+            case 'View all roles':
+                viewRoles();
+                break;
+            case 'View all employees':
+                viewEmployees();
+                break;
+            case 'Add a department':
+                addDepartment();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'Add an employee':
+                addEmployee();
+                break;
+            case 'Update an employee role':
+                updateEmployeeRole();
+                break;
+            case 'Exit':
+                db.end();
+                break;
+        }
+    })
+}
+
