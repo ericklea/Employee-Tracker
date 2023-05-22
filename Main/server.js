0eccc17422bf164cadb81cbcec7986a81a64db58
L E <erickson.leah.j@gmail.com>
2023-05-21 21:27:03 -0600

    input server.js code

--- Main/server.js
+++ Main/server.js
@@ -157,3 +157,43 @@ app.use((req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
+
+// Function to initialize app
+function init() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'What would you like to do?',
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
+        }
+    ])
+    .then((data) => {
+        switch (data.action) {
+            case 'View all departments':
+                viewDepartments();
+                break;
+            case 'View all roles':
+                viewRoles();
+                break;
+            case 'View all employees':
+                viewEmployees();
+                break;
+            case 'Add a department':
+                addDepartment();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'Add an employee':
+                addEmployee();
+                break;
+            case 'Update an employee role':
+                updateEmployeeRole();
+                break;
+        }
+    })
+}
+
+// Function call to initialize app
+init();
